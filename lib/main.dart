37373dfa8d0b38e33153d7535dd451c77e8ec129
SriKynanti <kynanti.ti@gmail.com>
2021-10-18 01:28:50 +0700

    Latest Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_chapter_3/input.dart';
+import 'package:flutter_chapter_3/result.dart';
+import 'package:flutter_chapter_3/convert.dart';
 
-class Result extends StatelessWidget {
-  const Result({
-    Key? key,
-    required double kelvin,
-    required double reamur,
-  })  : _kelvin = kelvin,
-        _reamur = reamur,
-        super(key: key);
+void main() {
+  runApp(MyApp());
+}
 
-  final double _kelvin;
-  final double _reamur;
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //variabel berubah
+  double _inputUSer = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+  // double _farenheit = 0;
+  final inputController = TextEditingController();
+
+  void hitungSuhu() {
+    setState(() {
+      _inputUSer = double.parse(inputController.text);
+      _kelvin = _inputUSer + 273;
+      _reamur = (4 / 5) * _inputUSer;
+      // _farenheit = (9 / 5) * _inputUSer + 32;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
             children: [
-              Container(
-                child: Text(
-                  'Suhu Dalam Kelvin',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: 10,
-                ),
-                child: Text(
-                  '$_kelvin',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
+              Input(inputController: inputController),
+              Result(kelvin: _kelvin, reamur: _reamur),
+              Conv(
+                convr: hitungSuhu,
               ),
             ],
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: Text(
-                  'Suhu dalam Reamor',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: 10,
-                ),
-                child: Text(
-                  '$_reamur',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
